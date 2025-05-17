0ff6f89c0f98f934e4a776b73bfb5f2840665b89
Nick2bad4u <20943337+Nick2bad4u@users.noreply.github.com>
2025-05-17 10:43:23 -0400

    refactor: update package declaration and version for Mark of Darkness Helper

--- src/main/java/com/markofdarknesshelper/MarkOfDarknessHelperConfig.java
+++ src/main/java/com/markofdarknesshelper/MarkOfDarknessHelperConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("markofdarknesshelper")
+@ConfigGroup("com/markofdarknesshelper")
 public interface MarkOfDarknessHelperConfig extends Config
 {
     @ConfigItem(
